a280d83efc656966638bc994b53d10baf1459f55
Ryan <ryangabriel.angelo@gmail.com>
2021-11-05 13:41:42 -0300

    Site Gamermania

--- js/script.js
+++ js/script.js
@@ -1,16 +1,53 @@
 $( document ).ready(function() {
-
+    
     $("#barras").click(function() {
-
+    
         if ($("#menu").hasClass("menu-ativo")) {
             $("#menu").removeClass("menu-ativo")
         } else {
             $("#menu").addClass("menu-ativo")
         }
-
+    
     })
 
+    // $('.logo').click(function(e){
+    //     e.preventDefault();
+    //     $('html, body').animate({
+    //         scrollTop: 0
+    //     }, 500)
+    // });
+    
+    // Slider
+   function slider(sliderName, velocidade) {
+	var sliderClass = '.' + sliderName,
+			activeClass = 'active',
+			rotate = setInterval(rotateSlide, velocidade);
+	
+	$(sliderClass + ' > :first').addClass(activeClass);
+
+	$(sliderClass).hover(function(){
+		clearInterval(rotate);
+	}, function() {
+		rotate = setInterval(rotateSlide, velocidade);
+	});
+	
+	function rotateSlide() {
+		var activeSlide = $(sliderClass + ' > .' + activeClass),
+				nextSlide = activeSlide.next();
+
+		if(nextSlide.length == 0) {
+			nextSlide = $(sliderClass + ' > :first');
+		}
+		activeSlide.removeClass(activeClass);
+		nextSlide.addClass(activeClass);
+	} 
+}
+slider('introducao', 2000);
+        
+
 })
+
+
 
 
 
